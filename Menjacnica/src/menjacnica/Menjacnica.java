1b836917e8418bf648e4c59f4da5e9c21badad66
milicaklaric <klaric42316ei@fon.bg.ac.rs>
2018-03-22 11:38:02 +0100

    Implementacija interfejsa

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -10,21 +10,35 @@ public class Menjacnica implements MenjacnicaInterfejs {
 	private LinkedList<Valuta> valute = new LinkedList<>();
 
 	@Override
-	public void dodajKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+	public void dodajKurs(Valuta v) throws Exception {
+		if (v == null || valute.contains(v))
+			throw new Exception("Valuta koju ste uneli je ili null ili vec postoji");
+		
+		valute.add(v);
+		
 	}
 
 	@Override
-	public void obrisiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+	public void obrisiKurs(Valuta v) throws Exception {
+		if (v == null || !valute.contains(v))
+			throw new Exception("Valuta koju ste uneli je ili null ili ne postoji");
+		
+		valute.remove(v);
 	}
 
 	@Override
-	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv) {
-		// TODO Auto-generated method stub
-		return null;
+	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv) throws Exception {
+		if (datum == null || naziv == null)
+			throw new Exception("Naziv i datum ne mogu biti null");
+		
+		Valuta v = new Valuta();
+		
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getNaziv().equals(naziv) && valute.get(i).getDatum().equals(datum)) {
+				v = valute.get(i);
+			}
+		}
+		
+		return v;
 	}
-
 }
--- Menjacnica/src/menjacnica/interfejs/MenjacnicaInterfejs.java
+++ Menjacnica/src/menjacnica/interfejs/MenjacnicaInterfejs.java
@@ -1,13 +1,14 @@
 package menjacnica.interfejs;
 
 import java.util.GregorianCalendar;
+
 import menjacnica.Valuta;
 
 public interface MenjacnicaInterfejs {
 	
-	public void dodajKurs(GregorianCalendar datum);
-	public void obrisiKurs(GregorianCalendar datum);
-	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv);
+	public void dodajKurs(Valuta v) throws Exception ;
+	public void obrisiKurs(Valuta v) throws Exception ;
+	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv) throws Exception ;
 	
 
 }
